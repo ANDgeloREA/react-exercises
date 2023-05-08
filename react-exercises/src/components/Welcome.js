097ee1152de4a94ab45288f439e83d2082cfe178
Andrea Angelo D'Onorio De Meo <andgelorea@gmail.com>
2023-05-08 22:03:17 +0200

    complete exercise 07

--- react-exercises/src/components/Welcome.js
+++ react-exercises/src/components/Welcome.js
@@ -1,10 +1,19 @@
 import React from "react"
 
 class Welcome extends React.Component {
-    super(props) {} 
     render() {
-        return <p>Welcome, {this.props.name}! Your age is {this.props.age}!</p>
+        return (
+            <>
+                <p>Welcome, <strong>{this.props.name}</strong>!</p>
+                <p>Your age is {this.props.age}!</p>
+            </>
+        )
     }
 }
+
+Welcome.defaultProps = {
+    name: "Andrea",
+    age: "29"
+  }
 
 export default Welcome;
