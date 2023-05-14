bcfdc8ded0a4ed905ed17ba6779284c1df08271d
Andrea Angelo D'Onorio De Meo <andgelorea@gmail.com>
2023-05-14 18:14:14 +0200

    complete exercise 18

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Counter from "./components/Counter";
+import ClickCounter from "./components/ClickCounter";
 
 function App() {
   return (
     <>
-      <Counter />
+      <ClickCounter />
     </>
   );
 }
