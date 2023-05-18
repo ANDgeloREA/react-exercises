b7f71f6be8c0668dcb4033a6179ccf47f0b1d726
Andrea Angelo D'Onorio De Meo <andgelorea@gmail.com>
2023-05-18 18:20:12 +0200

    complete exercise 23

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Login from "./components/Login";
+import UncontrolledLogin from "./components/UncontrolledLogin";
 
 function App() {
   return (
     <>
-      <Login />
+      <UncontrolledLogin />
     </>
   );
 }
