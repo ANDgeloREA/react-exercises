336bc917265d2ce8e0fc78a06fb44ea5c0d737dd
Andrea Angelo D'Onorio De Meo <andgelorea@gmail.com>
2023-05-21 19:47:58 +0200

    complete exercise 43

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,9 +1,23 @@
-import CarForm from "./components/CarForm";
+import { useState, } from "react";
+import { LanguageContext } from "./LanguageContext";
+import DisplayLanguage from "./components/DisplayLanguage";
 
 const App = () => {
+  const [language, setLanguage] = useState('en')
+
+  function handleChangeLanguage(event) {
+    setLanguage(event.target.value)
+  }
+
   return (
     <>
-      <CarForm />
+      <LanguageContext.Provider value={language}>
+        <select onChange={handleChangeLanguage} value={language}>
+          <option value='en'>English</option>
+          <option value='it'>Italiano</option>
+        </select>
+        <DisplayLanguage />
+      </LanguageContext.Provider>
     </>
   );
 }
