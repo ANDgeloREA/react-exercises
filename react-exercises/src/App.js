cc88a2c4be2445b06514684c2b10c89876b37dbc
Andrea Angelo D'Onorio De Meo <andgelorea@gmail.com>
2023-05-22 00:30:54 +0200

    complete exercise 48

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -5,6 +5,11 @@ import ShowGithubUser from "./components/ShowGithubUser";
 const App = () => {
   return (
     <>
+      <nav>
+        <Link to="/users/ANDgeloREA">Andrea</Link>
+        <Link to="/users/PuglisiAlfio">Alfio</Link>
+        <Link to="/users/gabrimarch3">Gabriel</Link>
+      </nav>
       <Routes>
         <Route path="users/:username" element={<ShowGithubUser />} />
       </Routes>
--- react-exercises/src/components/GithubUser.js
+++ react-exercises/src/components/GithubUser.js
@@ -3,18 +3,18 @@ import { useEffect, useState } from "react";
 const GithubUser = ({ username }) => {
     const [data, setData] = useState([]);
 
-    const fetchData = async () => {
-        try {
-            const response = await fetch(`https://api.github.com/users/${username}`);
-            const result = await response.json()
-            console.log(result)
-            setData(result)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
+    
     useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`https://api.github.com/users/${username}`);
+                const result = await response.json()
+                console.log(result)
+                setData(result)
+            } catch (error) {
+                console.log(error)
+            }
+        }
         fetchData(username)
     }, [username])
 
